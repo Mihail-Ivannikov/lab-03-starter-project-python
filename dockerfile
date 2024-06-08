820fbb608cf4364a5565f906af1a699ef154b435
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-06-08 04:43:53 +0300

    modify dockerfile and api

--- dockerfile
+++ dockerfile
@@ -4,6 +4,7 @@ RUN apk add --no-cache gcc musl-dev
 WORKDIR /app
 COPY requirements/backend.in /app/requirements/backend.in
 RUN pip install --no-cache-dir -r /app/requirements/backend.in
+RUN pip install --no-cache-dir -r /app/requirements.txt 
 COPY spaceship /app/spaceship
 COPY build /app/build
 EXPOSE 8000
--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from flask import Blueprint, jsonify
+import numpy as np
 
-router = APIRouter()
+api = Blueprint('api', __name__)
 
-
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@api.route('/matrix', methods=['GET'])
+def matrix():
+    matrix_a = np.random.randint(10, size=(10, 10))
+    matrix_b = np.random.randint(10, size=(10, 10))
+    product = np.dot(matrix_a, matrix_b)
+    return jsonify({
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    })
